63bf8f7ce074578b5c7c5c07ea7151203c33a590
adnanq2004 <aquayyum20@stuy.edu>
2020-11-01 17:30:39 -0500

    added string get function

--- SuperArray.java
+++ SuperArray.java
@@ -3,8 +3,8 @@ public class SuperArray{
 	private String[] data;
 	private int size;
 
-	public SuperArray() {
-		this.size = 10;
+	public SuperArray(int size) {
+		this.size = size;
 	}
 
 	public int size() {
@@ -12,13 +12,23 @@ public class SuperArray{
 	}
 
 	public boolean add(String element) {
-		String[] temp = new String[data.length+1];
-		for (int i = 0; i < data.length; i++) {
+		String[] temp = new String[size+1];
+		for (int i = 0; i < size; i++) {
 			temp[i] = data[i];
 		}
 		temp[temp.length-1] = element;
 		this.data = temp;
+		this.size = size++;
 		return true;
+	}
+
+	public String get(int index) {
+		if (index < size) {
+			return data[index];
+		}
+		else {
+			return "faulty index: please provide appropriate index";
+		}
 	}
 
 }
